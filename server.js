ddf63f56ea7b74a131ae2fdc353c58b9f9dab7a3
Jcom619 <joshuacompton@my.smccd.edu>
2021-04-15 07:25:23 -0700

    j

--- server.js
+++ server.js
@@ -7,23 +7,67 @@ const rowdyResults = rowdy.begin(app);
 const PORT = process.env.MY_SERVER_PORT || 8080;
 
 require("dotenv").config();
+app.use("/", express.static("public"));
 
-//? ======================================
-//!          create routes...
-//? ======================================
+//?|   ⚡︎   ⚡︎   ⚡︎   |☠︎|==========|☠︎|   ⚡︎   ⚡︎   ⚡︎   |
+//!|   ⚡︎   ⚡︎   ⚡︎   |☠︎| API Call |☠︎|   ⚡︎   ⚡︎   ⚡︎   |
+//?|   ⚡︎   ⚡︎   ⚡︎   |☠︎|==========|☠︎|   ⚡︎   ⚡︎   ⚡︎   |
+
+// let weather = {
+//      apiKey: "7a9e0d0b445ca1be17f7e626ad349281",
+//      fetchWeather: function (city) {
+//        fetch(
+//          "https://api.openweathermap.org/data/2.5/weather?q=" +
+//            city +
+//            "&appid=" +
+//            this.apiKey +
+//            "&units=imperial"
+//        )
+//          .then((response) => {
+//            if (!response.ok) {
+//              console.log("No weather found.");
+//              throw new Error("No weather found.");
+//            }
+//            return response.json();
+//          })
+//          .then((data) => this.displayWeather(data));
+// }
+//* ================================================================================================
+//*|   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎  ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎  ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎  ⚡︎   ⚡︎   ⚡︎  |
+//*|                                   setting middleware                                          |
+//*|   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎  ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎  ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎  ⚡︎   ⚡︎   ⚡︎  |
+//* ================================================================================================
+
+app.use("/images", express.static(__dirname + "/Style"));
+app.use(express.static(__dirname + "public"));
+
+//* ================================================================================================
+//*|   ⚡︎   ⚡︎   ⚡︎  ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎  ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎  ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎   ⚡︎  ⚡︎   ⚡︎   ⚡︎   |
+//* ================================================================================================
+
+//?|   ⚡︎   ⚡︎   ⚡︎   |☠︎|==========|☠︎|   ⚡︎   ⚡︎   ⚡︎   |
+//!|   ⚡︎   ⚡︎   ⚡︎   |☠︎|  Routes  |☠︎|   ⚡︎   ⚡︎   ⚡︎   |
+//?|   ⚡︎   ⚡︎   ⚡︎   |☠︎|==========|☠︎|   ⚡︎   ⚡︎   ⚡︎   |
 
 app.get("/", (req, res) => {
-  res.json();
+  pass;
 });
 
-console.log(PORT);
 app.get("/api", (req, res) => {
-  res.json("");
+  res.send("¿hola...Mundo?");
 });
 
-//? =====================================
+//?|   ⚡︎   ⚡︎   ⚡︎   |☠︎|==========|☠︎|   ⚡︎   ⚡︎   ⚡︎   |
+//!|   ⚡︎   ⚡︎   ⚡︎   |☠︎|          |☠︎|   ⚡︎   ⚡︎   ⚡︎   |
+//?|   ⚡︎   ⚡︎   ⚡︎   |☠︎|==========|☠︎|   ⚡︎   ⚡︎   ⚡︎   |
+
 app.listen(PORT, () => {
-  console.log(`secret server 007 is running on: ${PORT}... you got served!`);
   rowdyResults.print();
+  console.log(
+    ` \n\n   |  ⚡︎ |  ☠︎  |  ⚡︎ |  ☠︎  |  ⚡︎ |  ☠︎  |   ⚡︎ |  ☠︎  |  ⚡︎ |  ☠︎  | \n   --------------------------------------------------------------\n   | ${app.settings.views} |\n\n`
+  );
+  console.log(
+    `                          ☁︎ ☁︎ ☁︎ ☁︎ ☁︎ ☁︎ ☁︎\n\n                          you got served\n\n                               ${PORT}      \n\n                          ☔︎ ☔︎ ☔︎ ☔︎ ☔︎\n\n`
+  );
 });
-console.log(app.path());
+//|   ⚡︎   ⚡︎   ⚡︎  | ☠︎ | ⚡︎ | ☠︎ |  ☠︎  ⚡︎   ⚡︎   ⚡︎   ||   ⚡︎   ⚡︎   ⚡︎  | ☠︎ | ⚡︎ | ☠︎ |  ☠︎  ⚡︎   ⚡︎   ⚡︎   |?\\
